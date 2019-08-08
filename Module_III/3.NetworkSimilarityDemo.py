410fb113f91ebb46d2ef10d78fd5f4a0e0765422
Charles Huang <chiyuanh@microsoft.com>
2019-08-07 18:46:45 -0700

    Changed NetworkSimilarity notebook

--- Module_III/3.NetworkSimilarityDemo.py
+++ Module_III/3.NetworkSimilarityDemo.py
@@ -12,37 +12,84 @@ MagContainer = 'mag-2019-06-07'
 # Shared access signature of MAG Container
 MagSAS = ''
 
-# Network Similarity Resource path
-ResourcePath = 'ns/AffiliationCopaper.tsv'
-
 # COMMAND ----------
 
 mag = MicrosoftAcademicGraph(container=MagContainer, account=MagAccount, sas=MagSAS)
 Affiliations = mag.getDataframe('Affiliations')
-display(Affiliations)
+ConferenceSeries = mag.getDataframe('ConferenceSeries')
+FieldsOfStudy = mag.getDataframe('FieldsOfStudy')
+Journals = mag.getDataframe('Journals')
 
 # COMMAND ----------
 
+# Affiliation Similarity
+ResourcePath = 'ns/AffiliationEmbedding_d100.tsv'
 ns = NetworkSimilarity(resource=ResourcePath, container=MagContainer, account=MagAccount, sas=MagSAS)
 df = ns.getDataframe()
 display(df)
 
 # COMMAND ----------
 
-print(ns.getSimilarity(157347489, 78545622))
+id1 = 1290206253 # Microsoft
+id2 = 136199984 # Harvard University
+print(ns.getSimilarity(id1, id2))
 
 # COMMAND ----------
 
-aff = Affiliations.where(Affiliations.AffiliationId == 157347489)
-display(aff)
+topEntities = ns.getTopEntities(id1)
+topEntitiesWithName = topEntities.join(Affiliations, topEntities.EntityId == Affiliations.AffiliationId, 'inner') \
+    .select(Affiliations.AffiliationId, Affiliations.DisplayName, topEntities.EntityType, topEntities.Score)
+display(topEntitiesWithName)
 
 # COMMAND ----------
 
-topEntities = ns.getTopEntities(157347489)
+# Venues
+ResourcePath = 'ns/VenueEmbedding_d100.tsv'
+ns = NetworkSimilarity(resource=ResourcePath, container=MagContainer, account=MagAccount, sas=MagSAS)
+df = ns.getDataframe()
+#display(df)
+
+# COMMAND ----------
+
+id1 = 1130985203 # KDD
+id2 = 137773608 # Nature
+print(ns.getSimilarity(id1, id2))
+
+# COMMAND ----------
+
+topEntities = ns.getTopEntities(id1)
 display(topEntities)
 
 # COMMAND ----------
 
-top = topEntities.join(Affiliations, topEntities.EntityId == Affiliations.AffiliationId, 'inner') \
-    .select(Affiliations.AffiliationId, Affiliations.DisplayName, topEntities.EntityType, topEntities.Score)
-display(top)
+# Union conferences and journals to get venue dataframe
+conf = ConferenceSeries.select(ConferenceSeries.ConferenceSeriesId.alias('VenueId'), ConferenceSeries.NormalizedName, ConferenceSeries.DisplayName)
+jour = Journals.select(Journals.JournalId.alias('VenueId'), Journals.NormalizedName, Journals.DisplayName)
+venues = conf.union(jour)
+
+# COMMAND ----------
+
+topEntities = ns.getTopEntities(id1)
+topEntitiesWithName = topEntities.join(venues, topEntities.EntityId == venues.VenueId, 'inner') \
+    .select(venues.VenueId, venues.NormalizedName, venues.DisplayName, topEntities.EntityType, topEntities.Score)
+display(topEntitiesWithName)
+
+# COMMAND ----------
+
+# Fields of Study
+ResourcePath = 'ns/FosEmbedding_d100.tsv'
+ns = NetworkSimilarity(resource=ResourcePath, container=MagContainer, account=MagAccount, sas=MagSAS)
+df = ns.getDataframe()
+
+# COMMAND ----------
+
+id1 = 124101348 # Data mining
+id2 = 108583219 # Deep learning
+print(ns.getSimilarity(id1, id2))
+
+# COMMAND ----------
+
+topEntities = ns.getTopEntities(id1)
+topEntitiesWithName = topEntities.join(FieldsOfStudy, topEntities.EntityId == FieldsOfStudy.FieldOfStudyId, 'inner') \
+    .select(FieldsOfStudy.FieldOfStudyId, FieldsOfStudy.DisplayName, topEntities.EntityType, topEntities.Score)
+display(topEntitiesWithName)
